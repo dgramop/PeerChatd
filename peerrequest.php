d4a3f1babc7035445821c8c9517310fb4ebc6d29
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-27 21:18:00 -0400

    Create peerrequest.php

---
+++ peerrequest.php
@@ -0,0 +1,9 @@
+<?php
+/*
+This file is to be included
+
+It will go around requesting user information for a user who wants to login from this server. It will compare the information it recieves and, 
+if it is different from diffferent sources, will provide the possible options to the user as to which server to import from. By default it will democratically
+select information based on the most common and reinforced data, but this gives an advantage to any entity that owns enough servers
+*/
+?>
